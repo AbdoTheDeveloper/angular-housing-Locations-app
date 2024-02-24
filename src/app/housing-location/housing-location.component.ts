e52ab62723efcc86e86cb72a26e60f8971b9c032
EL KASBAJI Abdelhadi <akasbaji2@gmail.com>
2024-02-24 20:56:14 +0100

    remerging base code

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,22 +1,27 @@
-import { Component  , Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HousingLocation } from '../housing-location';
+import { HousingLocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   template: `
     <section class="listing">
-      <img [src]="housingLocation.photo" alt="Housing Location photo " class="listing-photo">
-      <h2 class ="listing-heading">{{housingLocation.name}}</h2>
-      <p class="listing-location">{{housingLocation.city}} ,  {{housingLocation.state}}</p>
-       </section>
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    </section>
   `,
-  styleUrls: ['./housing-location.component.css']
+  styleUrls: ['./housing-location.component.css'],
 })
-export class HousingLocationComponent {
-  @Input() housingLocation! : HousingLocation ; 
 
+export class HousingLocationComponent {
+
+  @Input() housingLocation!: HousingLocation;
 
 }
